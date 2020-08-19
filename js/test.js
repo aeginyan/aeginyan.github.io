3411d7b463bd0a598cd360bfe68761a0618027d4
aeginyan <artur.eginyan@gmail.com>
2020-08-18 22:19:28 -0700

    changes

--- js/test.js
+++ js/test.js
@@ -1,7 +1,18 @@
-const foo = () => {console.log("Arrow Hello World!")}
+const foo = () => console.log("Arrow Hello World!")
 foo();
 
 
-fetch('http://www.omdbapi.com/?i=tt0083658&apikey=20191f26').then(response => response.json()).then(data => console.log(data));
+fetch('http://www.omdbapi.com/?i=tt0083658&apikey=2019126').then(response => response.json()).then(data => console.log(data)).catch(error => console.log(error));
 
+const bar = async () => {
+    try {
+        let response = await fetch('http://www.omdbapi.com/?i=tt0083658&apikey=20191f26');
+        let data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+bar();
 
